227af8ea6e1541cd37b5a149aba2638a8d406fed
muhammetyigit <muhammettyiigit@gmail.com>
2025-05-07 17:01:40 +0300

    Initial Commit

---
+++ ReminderApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ReminderApp
+//
+//  Created by Muhammet Yiğit on 7.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
