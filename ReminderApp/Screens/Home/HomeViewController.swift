9f86002179eff93ef9d959b1de2f94f00c5f81ff
muhammetyigit <muhammettyiigit@gmail.com>
2025-05-07 17:51:44 +0300

    Implement MVVM file system, Add app icon to the project

---
+++ ReminderApp/Screens/Home/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  ReminderApp
+//
+//  Created by Muhammet Yiğit on 7.05.2025.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - UI Elements
+    
+    // MARK: - Properties
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Functions
+    
+    // MARK: - Actions
+}
---
+++ ReminderApp/Screens/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  ReminderApp
+//
+//  Created by Muhammet Yiğit on 7.05.2025.
+//
+
+import Foundation
